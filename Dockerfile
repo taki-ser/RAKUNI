b8ce9b173a822ef9894100532d5a857895ccbccb
taki-ser <rt12201311@gmail.com>
2024-08-29 14:05:36 +0900

    ファイル構造変更

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,16 @@ FROM node:lts
 
 # 作業ディレクトリを作成
 WORKDIR /usr/src/app
+
+# 依存関係のインストール
+COPY package*.json ./
+RUN yarn install
+
+# アプリケーションのソースコードをコピー
+COPY . .
+
+# 開発サーバーを起動
+CMD ["yarn", "start"]
+
+# アプリケーションが動作するポートを指定
+EXPOSE 3000
--- rakuni-app/src/App.tsx
+++ rakuni-app/src/App.tsx
@@ -10,9 +10,6 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <p>
-          RAKUNI
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
