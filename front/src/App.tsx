3e0760d088b7c6dfc449cd47c44424f59cd5683a
taki-ser <rt12201311@gmail.com>
2024-09-08 14:33:31 +0900

    Eduの追加と画像の追加

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -16,9 +16,10 @@ const App: React.FC = () => {
             <nav>
               <ul className="flex space-x-4">
                 <li><a href="#about">About</a></li>
-                <li><a href="#owner">Owner</a></li>
+                <li><a href="#staff">Staff</a></li>
                 <li><a href="#menu">Menu</a></li>
                 <li><a href="#access">Access</a></li>
+                <li><a href="#edu">Education</a></li>
               </ul>
             </nav>
           </div>
@@ -33,23 +34,49 @@ const App: React.FC = () => {
             <h3 className="text-3xl font-semibold mb-4">About</h3>
             <p className="text-lg">当店では、タイ古式/アロママッサージ/リフレ/ヘッドなど様々なメニューをご用意しております。本場タイの熟練技術を体感していただき、全身リラックスしていただけますと幸いです。ほっとできる癒しの空間をご提供いたします。</p>
             {/* <p className="text-lg">また、タイ古式マッサージの施術師の教育を行っています</p> */}
-            <img 
-              src="/assets/867335_0.jpg" 
-              alt="タイ古式マッサージの画像" 
-              className="mx-auto mt-6 object-cover w-1/2"
-            />
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+              <img 
+                src="/assets/083C3A3F-0EB6-4A84-BD19-9FD92D1AE943.jpg" 
+                alt="タイ古式マッサージの画像" 
+                className="mx-auto mt-6 object-cover"
+              />
+              <img 
+                src="/assets/867335_0.jpg" 
+                alt="タイ古式マッサージの画像" 
+                className="mx-auto mt-6 object-cover"
+              />
+              <img 
+                src="/assets/6C530DBB-656B-48AF-955D-ADD487A6B1A5.jpg" 
+                alt="タイ古式マッサージの画像" 
+                className="mx-auto mt-6 object-cover"
+              />
+            </div>
           </div>
         </section>
-        {/* Owner */}
-        <section id="owner" className="py-16 bg-gray-100">
+
+        {/* Staff */}
+        <section id="" className="py-16 bg-gray-100">
           <div className="container mx-auto text-center">
-            <h3 className="text-3xl font-semibold mb-4">Owner</h3>
-            <h4 className="text-2xl font-semibold mb-4">ルークミー</h4>
-            <img 
-              src="/assets/3A89685F-DDA4-4F1C-BBFC-756982909D4D.jpg" 
-              alt="タイ古式マッサージの画像" 
-              className="mx-auto mt-6 rounded-full w-48 h-48 object-cover shadow-lg"
-            />
+            <h3 className="text-3xl font-semibold mb-4">Staff</h3>
+            {/* <h4 className="text-2xl font-semibold mb-4">ルークミー</h4> */}
+            <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-4">
+              <div className="flex flex-col items-center">
+                <img 
+                  src="/assets/439631032_3190676324395994_4406625110278184235_n.jpg" 
+                  alt="ルークミーさんの画像" 
+                  className="mx-auto mt-6 rounded-full w-48 h-48 object-cover shadow-lg"
+                />
+                <h4 className="text-2xl mt-8">ルークミー</h4>
+              </div>
+              <div className="flex flex-col items-center">
+              <img 
+                src="/assets/867326.jpg" 
+                alt="プーさんの画像" 
+                className="mx-auto mt-6 rounded-full w-48 h-48 object-cover shadow-lg"
+              />
+              <h4 className="text-2xl mt-8">プー</h4>
+              </div>
+            </div>
             <p className="text-lg">明日への活力に繋げていただけるよう、精一杯お客様に寄り添った施術をさせていただきます！タイ古式が初めての方もぜひお越しください。</p>
           </div>
         </section>
@@ -58,6 +85,7 @@ const App: React.FC = () => {
         <section id="menu" className="py-16 bg-white">
           <div className="container mx-auto text-center">
             <h3 className="text-3xl font-semibold mb-4">Menu</h3>
+            <p className='text-lg'>タイの伝統的なマッサージでは、長い時間をかけてゆっくり全身を施術するのが特徴です。 局所的対処ではなく、2～3時間かけ、身体全体の血液とリンパ液の流れを良くすることで、体調の改善を図ることを目的とします。 肩が凝っているから肩を揉む、腰痛だから腰部だけをマッサージするという考えは、タイ古式マッサージにはありません。</p>
             {/* <ul className="text-lg space-y-4">
               <li>アロママッサージ - 60分</li>
               <li>指圧マッサージ - 60分</li>
@@ -65,7 +93,7 @@ const App: React.FC = () => {
             </ul> */}
             <img
               src="/assets/867336_0.jpg"
-              alt="タイ古式マッサージの画像"
+              alt="タイ古式マッサージのメニュー画像"
               className="mx-auto mt-6 "
             />
           </div>
@@ -101,6 +129,27 @@ const App: React.FC = () => {
           <p className="text-lg">090-6654-8491</p>
         </section>
 
+        {/* Education */}
+        <section id="edu" className="py-16 bg-white">
+          <div className="container mx-auto text-center">
+            <h3 className="text-3xl font-semibold mb-4">Education</h3>
+            <h4 className="text-2xl mt-8">タイ伝統医学協会のインストラクターによる教育</h4>
+            <p className="text-lg">RAKUNIには協会のインストラクターが所属しており、タイ古式マッサージ、オイルマッサージ、足ツボマッサージ等の教育を提供しています。</p>
+            <p className="text-lg">タイ古式マッサージを習得したい方は日本人でもタイ人でも受講可能です。詳細情報をご希望の方はお気軽にご連絡ください。</p>
+            <p className="text-lg">受講者にはタイ伝統医学協会の修了証が発行されます。</p>
+            <img 
+              src="assets/タイ伝統医学協会.jpg" 
+              alt="タイ伝統医学協会の画像" 
+              className="mx-auto mt-6 rounded-full w-48 h-48 object-cover shadow-lg" 
+               /> 
+            <img 
+              src="assets/2C0CDA65-0B79-4723-91CA-DC7263E5B9BA.jpg" 
+              alt="マッサージの受講終了生の画像" 
+              className="mx-auto mt-6 object-cover shadow-lg" 
+               />
+            {/* <h4 className="text-2xl mt-8">講師</h4>  */}
+          </div>
+        </section>
         {/* フッター */}
         <footer className="bg-primary text-white py-4">
           <div className="container mx-auto text-center">
